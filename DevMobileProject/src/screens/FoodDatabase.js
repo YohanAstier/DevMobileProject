cd63b2b1710a02e0c05489b65bb9b963045d6338
YohanAstier <yohan.astier@edu.esiee.fr>
2023-06-06 14:42:26 +0200

    step 1-3 create form

--- DevMobileProject/src/screens/FoodDatabase.js
+++ DevMobileProject/src/screens/FoodDatabase.js
@@ -1,15 +1,51 @@
-/* eslint-disable prettier/prettier */
 import React from 'react'
-import { Button, View, Text } from 'react-native'
+import { View, Text, TextInput, StyleSheet, Button } from 'react-native'
 
 const FoodDatabase = ({ navigation }) => {
+    const [food, onChangeFood] = React.useState('');
+    const onSubmit = () => {
+        alert(food)
+    }
+
     return (
-        <View
-            style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
-        >
-            <Text>FoodDatabase</Text>
+        <View style={styles.container}>
+            <View style={styles.subContainer}>
+                <Text style={styles.searchLabel}>
+                    Choisissez l'aliment à rechercher
+                </Text>
+            </View>
+            <View style={styles.subContainer}>
+                <TextInput
+                    style={styles.searchInput}
+                    placeholder="Nourriture"
+                    onChangeText={onChangeFood}
+                />
+                <Button title="Envoyer" onPress={onSubmit} />
+            </View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 20,
+        flexDirection: 'column',
+        justifyContent: 'center',
+    },
+    subContainer: {
+        rowGap: 10,
+        padding: 20,
+    },
+    searchLabel: {
+        fontSize: 18,
+    },
+    searchInput: {
+        borderWidth: 1,
+        borderRadius: 5,
+        width: '100%',
+        padding: 10,
+    },
+})
 
 export default FoodDatabase
